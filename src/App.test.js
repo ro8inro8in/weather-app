e50cb7a2da26d3d2cc6de82f1fab26176dfed4ff
Robin Edwards <robinedwards1987@gmail.com>
2021-02-04 16:50:23 +0000

    set up done last part failing

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Weather App link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Weather App/i);
+  expect(linkElement).toBeInTheDocument();
+});
