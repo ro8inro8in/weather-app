9af0001db2195fa69357a3cbfe142473594a7f2d
Robin Edwards <robinedwards1987@gmail.com>
2021-02-06 15:23:32 +0000

    Location Details Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import "../styles/App.css";
 import React from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-    </div>
-  );
-}
+const App = ({ location }) => {
+  const { city, country } = location;
+  return <LocationDetails city={city} country={country} />;
+};
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 export default App;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 
-test('renders Weather App link', () => {
+test('renders 'Weather App' link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Weather App/i);
+  const linkElement = screen.getByText(/'Weather App'/i);
   expect(linkElement).toBeInTheDocument();
 });
